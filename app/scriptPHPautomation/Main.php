263a4304a500dcca0a88c0c1a080ddcb836c5ef9
Francener Alezy <francener@paxglobalmedia.com>
2019-07-30 13:16:43 -0400

    Php parser

--- app/scriptPHPautomation/Main.php
+++ app/scriptPHPautomation/Main.php
@@ -1,5 +1,15 @@
 <?php
 
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter;
+use PhpParser\Node\Stmt;
+use PhpParser\{Node, NodeFinder};
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\NameContext;
+
 use App\scriptPHPautomation\CheckIfFolderExist;
 use App\scriptPHPautomation\detectIfileExist;
 use App\scriptPHPautomation\getCurrentPath;
@@ -12,6 +22,9 @@ use App\scriptPHPautomation\toRenameNameSpace;
 
 namespace App\scriptPHPautomation;
 
+
+use PhpParser\NameContext;
+use PhpParser\NodeFinder;
 
 /**
  * Class Main to automate move dependecy class use by plugin from OctoberCMS to Laravel
@@ -35,6 +48,13 @@ class Main
 
         // The full current plugin to convert to laravel
         $this->pluginFullPath = $this->octoberpluginPath.'/'.$this->pluginDirectoryName.'/'.$this->pluginName;
+
+        // Create an objet from php-parser to find Node like class
+        $this->nodeFinder = new NodeFinder;
+
+        // Create an objet from php-parser to find Node NameSpace
+        $this->nameSpace = new NameContext;
+
     }
 
     /**
@@ -49,9 +69,12 @@ class Main
         //Create The Folder of the plugin in Laravel if not exist
        $this->checkFolderExistence($this->pluginFullPath);
 
+        //Search get Parent Class for current class if exist one
+        $this->getAllParentClasses($className=null);
+
         // Search Class Declaration and move it to App/$pluginNAme/Class
         //Get The full dependency class
-        $this->findDepencyClass($className);
+        $this->getAllClassesExtendAnother($className=null);
 
     }
 
@@ -65,14 +88,88 @@ class Main
         return $checkFolder ;
     }
 
-    /**
-     * @param $className
-     * @return searchClassDeclaration
-     */
-    public function findDepencyClass($className) {
 
-        $dependency = new searchClassDeclaration($className);
-        return $dependency;
+    /**
+     * Find all class nodes.
+     * @param $stmts
+     * @return array|\PhpParser\Node[]
+     */
+    public function getAllClasses($stmts){
+
+        $classes = $this->nodeFinder->findInstanceOf($stmts, Node\Stmt\Class_::class);
+        return $classes;
+    }
+
+    /**
+     * Find all classes that extend another class
+     * @param $stmts
+     * @return array|\PhpParser\Node[]
+     */
+    public function getAllClassesExtendAnother($stmts)
+    {
+        $extendingClasses = $this->nodeFinder->find($stmts, function(Node $node) {
+            return $node instanceof Node\Stmt\Class_
+                && $node->extends !== null;
+        });
+        return $extendingClasses;
+    }
+
+    /**
+     * Find first class that has name $name
+     * @param $stmts
+     * @param $name
+     * @return \PhpParser\Node|null
+     */
+    public function getAClassbyNAme($name, $stmts)
+    {
+        $class = $this->nodeFinder->findFirst($stmts, function(Node $node) use ($name) {
+            return $node instanceof Node\Stmt\Class_
+                && $node->resolvedName->toString() === $name;
+        });
+
+        return $class;
+    }
+
+    /**
+     * Start a new namespace.
+     * This also resets the alias table.
+     * @param \PhpParser\Node\Name|null $namespace
+     * @return mixed
+     */
+    public function createNewNameSpace(Name $namespace = null)
+    {
+        $newNameSpace = $this->nameSpace->startNamespace($namespace);
+        return $newNameSpace;
+    }
+
+    /**
+     * Get current namespace.
+     * @return null|Name Namespace (or null if global namespace)
+     */
+    public function getCurrentNameSpace()
+    {
+        $currentNameSpace = $this->nameSpace->getNamespace();
+        return $currentNameSpace;
+    }
+
+    /**
+     * @param null $className
+     * @return string
+     */
+    public function getParentClass($className=null) :string
+    {
+        $theClass = get_parent_class($className);
+        return $theClass ;
+    }
+
+    /**
+     * @param null $className
+     * @return array
+     */
+    public function getAllParentClasses($className=null) :array
+    {
+        $allParents = class_parents(new $className);
+        return $allParents;
     }
 
 }
